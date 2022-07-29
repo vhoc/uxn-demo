cf9a72a84b7ff8c198dd3ae95829f9b13824f13b
Víctor Olvera <vhocar@gmail.com>
2022-07-29 09:59:02 -0700

    Finished navbar component

--- src/NavBar/NavBar.tsx
+++ src/NavBar/NavBar.tsx
@@ -1,15 +1,24 @@
-import React, { ReactNode, HTMLAttributes } from 'react'
+import React, { HTMLAttributes } from 'react'
 import { MenuItems } from '../MenuItems/MenuItems'
 import { Properties } from '../data'
 import { faTimes } from "@fortawesome/free-solid-svg-icons"
 
 import '../styles.css'
 
+
 export interface Props extends HTMLAttributes<HTMLDivElement> {
-    children?: ReactNode
+    /** Array of objects with the following scructure:
+     * [{ icon?, title, style?, action?, submenu?  }]  
+     * - icon: [Optional], Type: IconDefinition (FontAwesome)
+     * - title: [Mandatory], Type: string
+     * - style: [Optional], Type: object
+     * - action: [Optional], Type: Function
+     * - submenu: [Optional], Properties[] (Recursive)
+     */
     menuItems?: Properties[]
 }
 
+/** A Navigation bar with a multilevel dynamic dropdown menu. */
 export const NavBar = ({menuItems = [{ icon: faTimes, title: `No menus found` }], ...props}: Props) : JSX.Element => {
 
     //console.warn(menuItems)
